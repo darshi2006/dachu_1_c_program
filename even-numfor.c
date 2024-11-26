0be67e03a751415a1d950556dfae497688ec3a13
darshi2006 <dachu854@gmail.com>
2024-11-26 04:39:02 -1000

    Create even-numfor.c

---
+++ even-numfor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 2; i <= n; i += 2) {
+        printf("%d\n", i);
+    }
+printf("Darshan.M");
+    return 0;
+}
