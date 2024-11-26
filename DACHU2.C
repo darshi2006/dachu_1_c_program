aff31542e17359aa764e960c2086a99d93e16140
darshi2006 <dachu854@gmail.com>
2024-11-26 12:26:47 +0530

    Add files via upload

---
+++ DACHU2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int num;
+printf("Enter the number");
+scanf("%d",&num);
+if("num>0"){
+printf("The number is positive");
+}else if("num<0"){
+printf("the number is negative");
+}else{
+printf("The number is zero");
+}
+return 0;
+}
