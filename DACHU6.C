2d77b601190f6cc6e23c4f94c34f3b9741ecc7b4
darshi2006 <dachu854@gmail.com>
2024-11-26 12:52:49 +0530

    Create DACHU6.C

---
+++ DACHU6.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int marks;
+{
+printf("enter the number");
+}  
+scanf("%d",&marks);
+if (marks>=90)
+{   
+printf("the grade is A");
+}
+else if (marks>=80)
+{
+printf("the grade is B");
+}
+else if (marks>=50)
+{
+printf("the grade is C");
+}
+else
+{
+printf("the grade is F");
+}
+return 0;
+}
