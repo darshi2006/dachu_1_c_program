dd5c9869f13bd84d83f112b59ee2654fc47dfb3e
darshi2006 <dachu854@gmail.com>
2024-11-26 12:44:52 +0530

    Create DACHU4.C

---
+++ DACHU4.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+ float p,r,t,i;
+  printf("enter p,r,and t:");
+  scanf("%f %f %f ",&p,&r,&t);
+  i=(p*r*t)/100;
+  printf("simple intrest=%.2f\n",i);
+  return 0;
+}
