96784e55970a87ed606baef5ec3ffd97b5114e32
darshi2006 <dachu854@gmail.com>
2024-11-27 12:58:20 +0530

    Add files via upload

---
+++ 1DARRAY.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int numbers[5]={10,20,30,40,50};
+printf("Array element:\n");
+for(int i=0; i<5; i++){
+printf("%d",numbers[i]);
+}
+printf("Darshan.M");
+return 0;
+}
